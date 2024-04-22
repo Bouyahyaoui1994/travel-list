f6b27900e8f021a47a84e95b7d885e4bce2c612b
Bouyahyaoui1994 <bn18121994@outlook.com>
2024-04-22 23:46:27 +0100

    Adding an item

--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleItem(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false,
+    };
+    setItems((items) => [...items, newItem]);
+
+    setDescription("");
+    setQuantity(1);
+  }
   return (
-    <div className="App">
+    <div>
       <h1>🎄 FAR AWAY 🧳</h1>
-      <form className=".add-form">
+      <form onSubmit={handleItem}>
         <h3>What do you need for your 😍 trip?</h3>
-        <select>
-          {}
-          <option value="1">1</option>
+        <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+          {Array.from({ length: 20 }, (_, i) => {
+            return (
+              <option key={i + 1} value={i + 1}>
+                {i + 1}
+              </option>
+            );
+          })}
         </select>
-        <input type="text" placeholder="Item..." />
+        <input
+          type="text"
+          placeholder="Item..."
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button>ADD</button>
       </form>
+      {/* {items &&
+        items.map((item, index) => <p key={index}>{item.description}</p>)} */}
     </div>
   );
 }
